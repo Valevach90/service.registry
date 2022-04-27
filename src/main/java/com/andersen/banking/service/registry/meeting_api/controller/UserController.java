c382e8fca4d218f603a9c1a1f565e2bc2a5af087
a.sidorin <sidorinaleksei@yandex.ru>
2022-04-27 16:23:21 +0300

    add controller layer

---
+++ src/main/java/com/andersen/banking/service/registry/meeting_api/controller/UserController.java
@@ -0,0 +1,53 @@
+package com.andersen.banking.service.registry.meeting_api.controller;
+
+import com.andersen.banking.service.registry.meeting_api.dto.UserDto;
+import com.andersen.banking.service.registry.meeting_db.entities.UserEntity;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Controller for mapping users.
+ */
+@Tag(name = "User controller", description = "work with users")
+@RequestMapping(value = "/api/v1/")
+@RestController
+public interface UserController {
+
+    @Operation(summary = "Get all users",
+            description = "get all users information")
+    @GetMapping("/users")
+    List<UserDto> findAll();
+
+    @Operation(summary = "Get user by user id",
+            description = "get user information by user id")
+    @GetMapping("/users/{id}")
+    UserDto findUserById(@Parameter(description = "user id", required = true)
+                         @PathVariable Long id);
+
+    @Operation(summary = "Save user in database",
+            description = "save user in database")
+    @PostMapping("/users")
+    Optional<UserEntity> saveUser(@Parameter(description = "user", required = true)
+                         @RequestBody UserEntity user);
+
+    @Operation(summary = "Update user",
+            description = "update user by params in dto")
+    @PutMapping("/users/{id}")
+    void updateAddress(
+            @Parameter(description = "user id", required = true)
+            @PathVariable Long id,
+            @RequestBody UserDto user
+    );
+
+    @Operation(summary = "Delete user by user id",
+            description = "delete user information by user id")
+    @GetMapping("/users/{id}")
+    void deleteUserById(@Parameter(description = "user id", required = true)
+                         @PathVariable Long id);
+
+}
---
+++ src/main/java/com/andersen/banking/service/registry/meeting_impl/controller/UserControllerImpl.java
@@ -0,0 +1,83 @@
+package com.andersen.banking.service.registry.meeting_impl.controller;
+
+import com.andersen.banking.service.registry.meeting_api.controller.UserController;
+import com.andersen.banking.service.registry.meeting_api.dto.UserDto;
+import com.andersen.banking.service.registry.meeting_db.entities.UserEntity;
+import com.andersen.banking.service.registry.meeting_impl.exceptions.NotFoundException;
+import com.andersen.banking.service.registry.meeting_impl.service.local.UserService;
+import com.andersen.banking.service.registry.meeting_impl.service.processing.UserProcessingService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class UserControllerImpl implements UserController {
+
+    private final UserProcessingService userProcessingService;
+
+    private final UserService userService;
+
+    @Override
+    public List<UserDto> findAll() {
+        log.trace("Find all users");
+
+        List<UserDto> users = userProcessingService.findAllUsersDto();
+
+        log.trace("Return list of Addresses: {}", users);
+        return users;
+    }
+
+    @Override
+    public UserDto findUserById(Long id) {
+        log.trace("Find user by userId: {}", id);
+
+        UserDto userDto = userProcessingService.findByIdUserDto(id);
+
+        log.trace("Return address: {}", userDto);
+
+        return userDto;
+    }
+
+    @Override
+    public Optional<UserEntity> saveUser(UserEntity newUser) {
+        log.trace("Saving new user in database: {}", newUser);
+
+        userService.findById(newUser.getId()).ifPresent(user -> {
+            throw new RuntimeException("The user with id: " + newUser.getId() + " is already saved before");
+        });
+
+        Optional<UserEntity> savedUser = Optional.ofNullable(userService.saveUser(Optional.of(newUser)).orElseThrow(() -> {
+            throw new RuntimeException("The user with id " + newUser.getId() + " is successful saved");
+        }));
+
+        log.trace("Save new user success in database: {}", newUser);
+        return savedUser;
+    }
+
+    @Override
+    public void updateAddress(Long id, UserDto user) {
+        log.trace("Try to update user: {}", user);
+
+        userProcessingService.updateUser(id, user);
+
+        log.trace("Updated user success");
+
+    }
+
+    @Override
+    public void deleteUserById(Long id) {
+        log.trace("Delete user with id: {}", id);
+
+        Optional.ofNullable(userService.findById(id).orElseThrow(() -> {
+            throw new NotFoundException(NotFoundException.BY_ID);
+        }));
+
+        userService.deleteUser(id);
+        log.trace("Delete success user with id: {}", id);
+    }
+}
