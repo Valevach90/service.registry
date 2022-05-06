d74e9b7cd5944f3d15ffe6b4b9d51ab197460779
ag.vlad23 <ag.vlad23@gmail.com>
2022-05-06 12:57:02 +0300

    Add NotNull validation

--- src/main/java/com/andersen/banking/service/registry/meeting_api/dto/PassportDto.java
+++ src/main/java/com/andersen/banking/service/registry/meeting_api/dto/PassportDto.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 import static com.andersen.banking.service.registry.meeting_api.utils.OpenApiConstants.DESCRIPTION_ADDRESS_ID;
@@ -42,6 +43,7 @@ public class PassportDto {
 
     @Schema(description = DESCRIPTION_PASSPORT_ID, example = EXAMPLE_LONG, defaultValue = EXAMPLE_LONG)
     @JsonProperty("id")
+    @NotNull
     private Long id;
 
     @Schema(description = DESCRIPTION_USER_ID, example = EXAMPLE_LONG, defaultValue = EXAMPLE_LONG)
@@ -55,35 +57,43 @@ public class PassportDto {
     @Schema(description = DESCRIPTION_PASSPORT_SERIAL_NUMBER, example = EXAMPLE_PASSPORT_SERIAL_NUMBER,
             defaultValue = EXAMPLE_PASSPORT_SERIAL_NUMBER)
     @JsonProperty("serialNumber")
+    @NotNull
     private String serialNumber;
 
     @Schema(description = DESCRIPTION_PASSPORT_CODE, example = EXAMPLE_PASSPORT_CODE, defaultValue = EXAMPLE_PASSPORT_CODE)
     @JsonProperty("passportCode")
+    @NotNull
     private String passportCode;
 
     @Schema(description = DESCRIPTION_PASSPORT_DIVISION_CODE, example = EXAMPLE_PASSPORT_DIVISION_CODE,
             defaultValue = EXAMPLE_PASSPORT_DIVISION_CODE)
     @JsonProperty("divisionCode")
+    @NotNull
     private String divisionCode;
 
     @Schema(description = DESCRIPTION_PASSPORT_BIRTHDAY, example = EXAMPLE_DATE, defaultValue = EXAMPLE_DATE)
     @JsonProperty("birthday")
+    @NotNull
     private LocalDate birthday;
 
     @Schema(description = DESCRIPTION_PASSPORT_DATE_ISSUE, example = EXAMPLE_DATE, defaultValue = EXAMPLE_DATE)
     @JsonProperty("dateIssue")
+    @NotNull
     private LocalDate dateIssue;
 
     @Schema(description = DESCRIPTION_PASSPORT_TERMINATION_DATE, example = EXAMPLE_DATE, defaultValue = EXAMPLE_DATE)
     @JsonProperty("terminationDate")
+    @NotNull
     private LocalDate terminationDate;
 
     @Schema(description = DESCRIPTION_FIRST_NAME, example = EXAMPLE_FIRST_NAME, defaultValue = EXAMPLE_FIRST_NAME)
     @JsonProperty("firstName")
+    @NotNull
     private String firstName;
 
     @Schema(description = DESCRIPTION_LAST_NAME, example = EXAMPLE_LAST_NAME, defaultValue = EXAMPLE_LAST_NAME)
     @JsonProperty("lastName")
+    @NotNull
     private String lastName;
 
     @Schema(description = DESCRIPTION_PATRONYMIC, example = EXAMPLE_PATRONYMIC, defaultValue = EXAMPLE_PATRONYMIC)
@@ -93,6 +103,7 @@ public class PassportDto {
     @Schema(description = DESCRIPTION_PASSPORT_DEPARTMENT_ISSUED, example = EXAMPLE_PASSPORT_DEPARTMENT_ISSUED,
             defaultValue = EXAMPLE_PASSPORT_DEPARTMENT_ISSUED)
     @JsonProperty("departmentIssued")
+    @NotNull
     private String departmentIssued;
 
     @Schema(description = DESCRIPTION_PASSPORT_BORN_PLACE, example = EXAMPLE_PASSPORT_BORN_PLACE,
