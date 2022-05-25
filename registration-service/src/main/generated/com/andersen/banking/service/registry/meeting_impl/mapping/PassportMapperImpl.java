5d435bd73d7fb575f416d5353a0c4810a5feccc4
Siarhei Yurevich <s.yurevich@andersenlab.com>
2022-05-26 00:41:00 +0300

    auto mappers implementation

--- registration-service/src/main/generated/com/andersen/banking/service/registry/meeting_impl/mapping/PassportMapperImpl.java
+++ registration-service/src/main/generated/com/andersen/banking/service/registry/meeting_impl/mapping/PassportMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-05-18T17:20:03+0300",
+    date = "2022-05-19T13:30:31+0300",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.2 (Oracle Corporation)"
 )
 @Component
