98cf3d8d37871dae0d549dd2c4eeeb7184b51afb
Siarhei Yurevich <s.yurevich@andersenlab.com>
2022-05-27 09:41:55 +0300

    corrected integration tests

--- registration-service/src/main/generated/com/andersen/banking/service/registry/meeting_impl/mapping/ErrorMapperImpl.java
+++ registration-service/src/main/generated/com/andersen/banking/service/registry/meeting_impl/mapping/ErrorMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-05-19T13:30:32+0300",
+    date = "2022-05-26T18:47:40+0300",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.2 (Oracle Corporation)"
 )
 @Component
