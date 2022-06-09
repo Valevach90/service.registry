228ff47cbfff0597bad0771a61201aa84de137fd
Siarhei Yurevich <s.yurevich@andersenlab.com>
2022-06-09 09:33:31 +0300

    without security

--- registration-service/src/main/java/com/andersen/banking/service/registry/meeting_impl/security/SecurityConfig.java
+++ registration-service/src/main/java/com/andersen/banking/service/registry/meeting_impl/security/SecurityConfig.java
@@ -28,10 +28,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests(authorizeRequests -> authorizeRequests
-                .antMatchers("/api/v1/auth").authenticated()
-                .antMatchers("/api/v1/users").hasRole("ADMIN")
-                .antMatchers("/api/v1/passport").hasRole("ADMIN")
-                .antMatchers("/api/v1/addresses").hasRole("ADMIN")
+                //.antMatchers("/api/v1/auth").authenticated()
+                //.antMatchers("/api/v1/users").hasRole("ADMIN")
+                //.antMatchers("/api/v1/passport").hasRole("ADMIN")
+                //.antMatchers("/api/v1/addresses").hasRole("ADMIN")
+                .antMatchers("/api/v1/**").permitAll()
                 .anyRequest().authenticated()).oauth2ResourceServer(
                 oauth2ResourceServer -> oauth2ResourceServer.jwt(
                         jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())
