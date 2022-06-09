9b41a9db2b12ee92e874d2ba455afa991c249b65
Siarhei Yurevich <s.yurevich@andersenlab.com>
2022-06-09 16:14:09 +0300

    with access to swagger

--- registration-service/src/main/java/com/andersen/banking/service/registry/meeting_impl/security/SecurityConfig.java
+++ registration-service/src/main/java/com/andersen/banking/service/registry/meeting_impl/security/SecurityConfig.java
@@ -34,7 +34,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 //.antMatchers("/api/v1/passport").hasRole("ADMIN")
                 //.antMatchers("/api/v1/addresses").hasRole("ADMIN")
                 .antMatchers("/api/v1/**").permitAll()
-                .antMatchers("/swagger-ui/**", "/api-docs**").permitAll()
                 .anyRequest().authenticated()).oauth2ResourceServer(
                 oauth2ResourceServer -> oauth2ResourceServer.jwt(
                         jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())
