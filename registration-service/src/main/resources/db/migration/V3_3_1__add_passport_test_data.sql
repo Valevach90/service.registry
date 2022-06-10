5eb725882bf1dff79eb3a479fd0eb694f6111c25
Roman Evlampiev <roman_evlampiev@epam.com>
2022-06-10 10:45:58 +0300

    added script for passport table change

--- registration-service/src/main/resources/db/migration/V3_3_1__add_passport_test_data.sql
+++ registration-service/src/main/resources/db/migration/V3_3_1__add_passport_test_data.sql
@@ -1,9 +1,9 @@
-INSERT INTO passport(user_id, address_id, serial_number, passport_code, birthday, date_issue,
+INSERT INTO passport(user_id, address_id, serial_number, passport_code, division_code, birthday, date_issue,
                      termination_date,
                      first_name, last_name, patronymic, born_place, department_issued)
-VALUES (3, 1, 13, 1337771, '2000-04-13', '2018-04-13', '2028-04-13', 'hehe', 'hoho', 'hihi', 'here',
+VALUES (3, 1, 13377, 133777, 1337777, '2000-04-13', '2018-04-13', '2028-04-13', 'hehe', 'hoho', 'hihi', 'here',
         'there'),
-       (2, 2, 14, 1338882, '2000-04-13', '2018-04-13', '2028-04-13', 'coolName', 'coolLastName',
+       (2, 2, 13388, 133888, 1338888, '2000-04-13', '2018-04-13', '2028-04-13', 'coolName', 'coolLastName',
         'patronymic', 'there', 'here'),
-       (1, 3, 15, 1339998, '2000-04-13', '2018-04-13', '2028-04-13', 'hehe', 'hoho', 'hihi', 'home',
+       (1, 3, 13399, 133999, 1339999, '2000-04-13', '2018-04-13', '2028-04-13', 'hehe', 'hoho', 'hihi', 'home',
         'zenit');
