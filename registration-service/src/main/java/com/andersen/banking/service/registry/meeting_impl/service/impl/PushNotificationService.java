0ef3db2a3f9272c4023f01f1e59c4357266df278
Roman Evlampiev <roman_evlampiev@epam.com>
2022-06-19 14:26:17 +0300

    created push notification endpoint

---
+++ registration-service/src/main/java/com/andersen/banking/service/registry/meeting_impl/service/impl/PushNotificationService.java
@@ -0,0 +1,22 @@
+package com.andersen.banking.service.registry.meeting_impl.service.impl;
+
+import com.andersen.banking.service.registry.meeting_api.dto.PushNotificationRequest;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@AllArgsConstructor
+public class PushNotificationService {
+
+  private FCMService fcmService;
+
+  public void sendPushNotificationToToken(PushNotificationRequest request) {
+    try {
+      fcmService.sendMessageToToken(request);
+    } catch (Exception e) {
+      log.error(e.getMessage());
+    }
+  }
+}
