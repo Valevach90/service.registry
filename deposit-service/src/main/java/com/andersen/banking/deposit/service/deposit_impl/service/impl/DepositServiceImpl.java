4c85c1fcf330a740e4f372a3394c05b04ac45d1b
Siarhei Yurevich <s.yurevich@andersenlab.com>
2022-06-29 12:30:25 +0300

    add service layer to deposit

---
+++ deposit-service/src/main/java/com/andersen/banking/deposit/service/deposit_impl/service/impl/DepositServiceImpl.java
@@ -0,0 +1,80 @@
+package com.andersen.banking.deposit.service.deposit_impl.service.impl;
+import com.andersen.banking.deposit.service.deposit_db.entities.Deposit;
+import com.andersen.banking.deposit.service.deposit_db.repositories.DepositRepository;
+import com.andersen.banking.deposit.service.deposit_impl.exceptions.NotFoundException;
+import com.andersen.banking.deposit.service.deposit_impl.service.DepositService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.Optional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class DepositServiceImpl implements DepositService {
+
+    private final DepositRepository depositRepository;
+
+    @Override
+    @Transactional
+    public Deposit create(Deposit deposit) {
+        log.info("Creating deposit: {}", deposit);
+
+        deposit.setId(null);
+
+        Deposit savedDeposit = depositRepository.save(deposit);
+
+        log.info("Created deposit: {}", savedDeposit);
+        return savedDeposit;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<Deposit> findById(Long id) {
+        log.info("Find deposit by id: {}", id);
+
+        Optional<Deposit> deposit = depositRepository.findById(id);
+
+        log.info("Found deposit by id: {}", deposit);
+        return deposit;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Page<Deposit> findAll(Pageable pageable) {
+        log.info("Find all deposits for pageable: {}", pageable);
+
+        Page<Deposit> pageOfDeposits = depositRepository.findAll(pageable);
+
+        log.info("Found {} deposits", pageOfDeposits.getContent().size());
+        return pageOfDeposits;
+    }
+
+    @Override
+    @Transactional
+    public void update(Deposit deposit) {
+        log.info("Updating deposit: {}", deposit);
+
+        Deposit foundDeposit = depositRepository.findById(deposit.getId())
+                .orElseThrow(() -> new NotFoundException(Deposit.class, deposit.getId()));
+
+        depositRepository.save(deposit);
+
+        log.info("Deposit: {} updated to version: {}", foundDeposit, deposit);
+    }
+    @Override
+    @Transactional
+    public void deleteById(Long id) {
+        log.info("Deleting deposit with id: {}", id);
+
+        Deposit foundDeposit = depositRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException(Deposit.class, id));
+
+        depositRepository.deleteById(id);
+
+        log.info("Deleted deposit: {}", foundDeposit);
+    }
+}
