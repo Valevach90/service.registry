d13cd276ac7bfd52c935537503472ddf0dbf6d4a
Siarhei Yurevich <s.yurevich@andersenlab.com>
2022-07-15 11:36:01 +0300

    replacing pathvariables with requesrparams

--- information-service/src/main/java/com/andersen/banking/meeting_api/controller/FileStorageController.java
+++ information-service/src/main/java/com/andersen/banking/meeting_api/controller/FileStorageController.java
@@ -25,15 +25,16 @@ public interface FileStorageController {
 
     @Operation(summary = "Get file download link",
             description = "get file download link by file name")
-    @GetMapping(value = "/link/{name}")
+    @GetMapping(value = "/link") //{name}
     String getFileDownloadLink(
             @Parameter(description = "file name", required = true)
-            @PathVariable ("name") String name);
+            //@PathVariable ("name") String name);
+            @RequestParam ("name") String name);
 
-    @Operation(summary = "Get files download links",
-            description = "get files download links for series of files names")
-    @GetMapping(value = "/info/{seriesOfFilesNames}")
+    @Operation(summary = "Get file(s) info with download link(s)",
+            description = "get file(s) info with download link(s)")
+    @GetMapping(value = "/info")
     List<FileInfoDto> getFilesDownloadLinks(
-            @Parameter(description = "series of files names", required = true)
-            @PathVariable ("seriesOfFilesNames") String... seriesOfFilesNames);
+            @Parameter(description = "file(s) name(s)", required = true)
+            @RequestParam ("name") String... name);
 }
