b2b22be733d0646e0f877f7f70ab3664a63cb645
Siarhei Yurevich <s.yurevich@andersenlab.com>
2022-07-18 10:41:00 +0300

    add app properties

--- information-service/src/main/generated/com/andersen/banking/meeting_impl/mapping/FileInfoMapperImpl.java
+++ information-service/src/main/generated/com/andersen/banking/meeting_impl/mapping/FileInfoMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-07-14T03:54:05+0300",
+    date = "2022-07-15T11:15:20+0300",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.2 (Oracle Corporation)"
 )
 @Component
