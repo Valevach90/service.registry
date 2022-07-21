e2dfe703b5c6fc0b40ea37e797a7542ec4f8a364
Siarhei Yurevich <s.yurevich@andersenlab.com>
2022-07-21 04:56:56 +0300

    integration test, h2

---
+++ deposit-service/src/main/generated/com/andersen/banking/deposit/service/deposit_impl/mapping/DepositTypeMapperImpl.java
@@ -0,0 +1,43 @@
+package com.andersen.banking.deposit.service.deposit_impl.mapping;
+
+import com.andersen.banking.deposit.service.deposit_api.dto.DepositTypeDto;
+import com.andersen.banking.deposit.service.deposit_db.entities.DepositType;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2022-07-19T01:19:45+0300",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.2 (Oracle Corporation)"
+)
+@Component
+public class DepositTypeMapperImpl implements DepositTypeMapper {
+
+    @Override
+    public DepositTypeDto toDepositTypeDto(DepositType depositType) {
+        if ( depositType == null ) {
+            return null;
+        }
+
+        DepositTypeDto depositTypeDto = new DepositTypeDto();
+
+        depositTypeDto.setId( depositType.getId() );
+        depositTypeDto.setName( depositType.getName() );
+
+        return depositTypeDto;
+    }
+
+    @Override
+    public DepositType toDepositType(DepositTypeDto depositTypeDto) {
+        if ( depositTypeDto == null ) {
+            return null;
+        }
+
+        DepositType depositType = new DepositType();
+
+        depositType.setId( depositTypeDto.getId() );
+        depositType.setName( depositTypeDto.getName() );
+
+        return depositType;
+    }
+}
---
+++ deposit-service/src/main/generated/com/andersen/banking/deposit/service/deposit_impl/mapping/TransferMapperImpl.java
@@ -0,0 +1,81 @@
+package com.andersen.banking.deposit.service.deposit_impl.mapping;
+
+import com.andersen.banking.deposit.service.deposit_api.dto.TransferDto;
+import com.andersen.banking.deposit.service.deposit_db.entities.Transfer;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2022-07-19T01:19:45+0300",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.2 (Oracle Corporation)"
+)
+@Component
+public class TransferMapperImpl implements TransferMapper {
+
+    @Override
+    public TransferDto toTransferDto(Transfer transfer) {
+        if ( transfer == null ) {
+            return null;
+        }
+
+        TransferDto transferDto = new TransferDto();
+
+        transferDto.setId( transfer.getId() );
+        transferDto.setFromCardNumber( transfer.getFromCardNumber() );
+        transferDto.setToCardNumber( transfer.getToCardNumber() );
+        transferDto.setAmount( transfer.getAmount() );
+        transferDto.setDate( transfer.getDate() );
+        transferDto.setSuccessStatus( transfer.getSuccessStatus() );
+
+        return transferDto;
+    }
+
+    @Override
+    public Transfer toTransfer(TransferDto transferDto) {
+        if ( transferDto == null ) {
+            return null;
+        }
+
+        Transfer transfer = new Transfer();
+
+        transfer.setId( transferDto.getId() );
+        transfer.setFromCardNumber( transferDto.getFromCardNumber() );
+        transfer.setToCardNumber( transferDto.getToCardNumber() );
+        transfer.setAmount( transferDto.getAmount() );
+        transfer.setDate( transferDto.getDate() );
+        transfer.setSuccessStatus( transferDto.getSuccessStatus() );
+
+        return transfer;
+    }
+
+    @Override
+    public List<Transfer> toTransfersDto(List<TransferDto> transfersDto) {
+        if ( transfersDto == null ) {
+            return null;
+        }
+
+        List<Transfer> list = new ArrayList<Transfer>( transfersDto.size() );
+        for ( TransferDto transferDto : transfersDto ) {
+            list.add( toTransfer( transferDto ) );
+        }
+
+        return list;
+    }
+
+    @Override
+    public List<TransferDto> toTransfers(List<Transfer> transfers) {
+        if ( transfers == null ) {
+            return null;
+        }
+
+        List<TransferDto> list = new ArrayList<TransferDto>( transfers.size() );
+        for ( Transfer transfer : transfers ) {
+            list.add( toTransferDto( transfer ) );
+        }
+
+        return list;
+    }
+}
