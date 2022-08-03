2f19ddbb26438262188161ec821264bfaa3a716b
Siarhei Yurevich <s.yurevich@andersenlab.com>
2022-08-03 13:57:48 +0300

    return mappers versions

--- registration-service/src/main/generated/com/andersen/banking/service/registry/meeting_impl/mapping/NotificationMapperImpl.java
+++ registration-service/src/main/generated/com/andersen/banking/service/registry/meeting_impl/mapping/NotificationMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-08-02T17:28:58+0300",
+    date = "2022-07-22T09:11:42+0300",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.2 (Oracle Corporation)"
 )
 @Component
