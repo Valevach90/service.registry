ced63301477ced697525683f755798a4066e6584
Siarhei Yurevich <s.yurevich@andersenlab.com>
2022-08-04 02:30:37 +0300

    add deposit api to gateway

--- deposit-service/src/main/java/com/andersen/banking/DepositServiceApplication.java
+++ deposit-service/src/main/java/com/andersen/banking/DepositServiceApplication.java
@@ -2,12 +2,13 @@ package com.andersen.banking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DepositServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DepositServiceApplication.class, args);
 	}
-
 }
