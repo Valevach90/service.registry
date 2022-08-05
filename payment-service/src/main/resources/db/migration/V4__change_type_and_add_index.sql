c63ad261af0e92a6f57a1191d520a17b940d23d8
Fulkin <sea-xv@mail.ru>
2022-08-05 13:53:10 +0500

    MRN-460 change type

---
+++ payment-service/src/main/resources/db/migration/V4__change_type_and_add_index.sql
@@ -0,0 +1,5 @@
+UPDATE payment_service.type_card
+SET type_name='SILVER'
+WHERE type_name='STANDARD';
+
+CREATE UNIQUE INDEX type_card_multi_idx ON payment_service.type_card (payment_system, type_name);
