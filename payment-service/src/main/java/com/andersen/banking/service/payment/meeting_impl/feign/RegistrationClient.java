51515f8367ce20d7ca581a389286e3c87d07ee6d
Mariia Semeniuk <m.semeniuk@andersenlab.com>
2022-08-11 12:37:28 +0000

    MRN-416 Add an api to select a source card and a recipient card of funds.

---
+++ payment-service/src/main/java/com/andersen/banking/service/payment/meeting_impl/feign/RegistrationClient.java
@@ -0,0 +1,12 @@
+package com.andersen.banking.service.payment.meeting_impl.feign;
+
+import com.andersen.banking.service.payment.meeting_impl.feign.dto.PersonalDataDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient(name = "api-gateway")
+public interface RegistrationClient {
+
+    @GetMapping("/api/v1/users/data")
+    PersonalDataDto getUserPersonalData();
+}
