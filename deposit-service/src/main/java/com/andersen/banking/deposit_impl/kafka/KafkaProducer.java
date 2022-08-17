0fc1923b8d9358b4b687defd29c6160d4c23e0d8
Valeria Bozhko <whlerwn@yandex.ru>
2022-08-17 20:53:03 +0300

    Add test for deposit service

--- deposit-service/src/main/java/com/andersen/banking/deposit_impl/kafka/KafkaProducer.java
+++ deposit-service/src/main/java/com/andersen/banking/deposit_impl/kafka/KafkaProducer.java
@@ -1,7 +1,6 @@
 package com.andersen.banking.deposit_impl.kafka;
 
 import com.andersen.banking.deposit_api.dto.messages.AccruedAmount;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
