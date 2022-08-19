487603e7ec65548c7d2083282d29be48120750fa
Siarhei Yurevich <s.yurevich@andersenlab.com>
2022-08-19 12:47:52 +0300

    delete extra matcher

--- registration-service/src/main/java/com/andersen/banking/service/registry/meeting_impl/util/ValidationUtil.java
+++ registration-service/src/main/java/com/andersen/banking/service/registry/meeting_impl/util/ValidationUtil.java
@@ -9,18 +9,15 @@ public class ValidationUtil {
 
     private ValidationUtil() {}
 
-    private static final String PASSWORD_REGEX = "^(?=.*[0-9]*)(?=.*[a-z]*)(?=.*[A-Z]*)(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]*)\\S{6,20}$";
-    private static final String CYRILLIC_CHARACTERS_REGEX = ".*[А-Яа-я]+.*";
+    private static final String PASSWORD_REGEX = "^[0-9a-zA-Z!#$%&'()*+,-./:;<=>?@\\[\\]^_`{|}~]{6,20}$";
 
     private static final Pattern passwordPattern = Pattern.compile(PASSWORD_REGEX);
-    private static final Pattern cyrillicCharacterPattern = Pattern.compile(CYRILLIC_CHARACTERS_REGEX, Pattern.UNICODE_CHARACTER_CLASS);
 
     public static boolean isPasswordValid(String password) {
 
         Matcher passwordMatcher = passwordPattern.matcher(password);
-        Matcher cyrillicCharacterMatcher = cyrillicCharacterPattern.matcher(password);
 
-        if (!cyrillicCharacterMatcher.matches() && passwordMatcher.matches()) {
+        if (passwordMatcher.matches()) {
             return true;
         }
         return false;
