d64381376f41324ed8eb7f6faa69254bbd5b0ebc
a.dzyubak <GxXqLoLa2SED>
2022-08-19 18:02:20 +0300

    update gradle files + migration script

--- transfer-service/src/main/resources/db/migration/V2__ddl.sql
+++ transfer-service/src/main/resources/db/migration/V2__ddl.sql
@@ -66,13 +66,3 @@ ALTER TABLE transfer
 ALTER TABLE transfer
     ADD CONSTRAINT FK_TRANSFER_ON_SOURCE_PAYMENT_TYPE FOREIGN KEY (source_payment_type_id) REFERENCES payment_type (id);
 
-
-INSERT INTO currency (created_by, created_date, deleted, last_modified_by, last_modified_date, description, name)
-VALUES (current_user, now(), false, current_user, now(), 'United States Dollar', 'USD');
-INSERT INTO currency (created_by, created_date, deleted, last_modified_by, last_modified_date, description, name)
-VALUES (current_user, now(), false, current_user, now(), 'European Union', 'EURO');
-
-INSERT INTO payment_type (created_by, created_date, deleted, last_modified_by, last_modified_date, description, name)
-VALUES (current_user, now(), false, current_user, now(), 'A payment card issued by a bank.', 'CARD');
-INSERT INTO payment_type (created_by, created_date, deleted, last_modified_by, last_modified_date, description, name)
-VALUES (current_user, now(), false, current_user, now(), 'A deposit is money held by a bank', 'DEPOSIT');
