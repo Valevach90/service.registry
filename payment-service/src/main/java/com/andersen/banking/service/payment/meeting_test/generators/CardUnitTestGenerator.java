43f8a20b24e883ec9f1c3521c340bde6f7ed198e
macp <->
2022-08-23 17:50:05 +0300

    Merge branch 'feature/MRN-553_add_numbers_and_amount_to_cardResponseDto'

--- payment-service/src/main/java/com/andersen/banking/service/payment/meeting_test/generators/CardUnitTestGenerator.java
+++ payment-service/src/main/java/com/andersen/banking/service/payment/meeting_test/generators/CardUnitTestGenerator.java
@@ -6,6 +6,7 @@ import com.andersen.banking.service.payment.meeting_db.entities.Card;
 import com.andersen.banking.service.payment.meeting_db.entities.TypeCard;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -25,73 +26,96 @@ public class CardUnitTestGenerator {
         return card;
     }
 
-  public static CardUpdateDto populateCardUpdateDto() {
-    CardUpdateDto cardUpdateDto = new CardUpdateDto();
-    cardUpdateDto.setId(17L);
-    cardUpdateDto.setAccountId(5L);
-    cardUpdateDto.setFirstTwelveNumbers("123456789012");
-    cardUpdateDto.setLastFourNumbers("4567");
-    cardUpdateDto.setValidFromDate(LocalDate.of(2021, 10, 23));
-    cardUpdateDto.setExpireDate(LocalDate.of(2024, 10, 23));
-    cardUpdateDto.setHolderName("Ivanov Ivan Ivanovich");
-    cardUpdateDto.setPaymentSystem("VISA");
-    cardUpdateDto.setTypeName("SILVER");
-    return cardUpdateDto;
-  }
+    public static Card populateCardIncludeAccountWithBalanceAndCurrency() {
+        Card card = populateCard();
+        card.setAccount(populateAccountWithBalanceAndCurrency(new Account()));
 
-  public static CardRegistrationDto populateCardRegistrationDto() {
-    CardRegistrationDto cardRegistrationDto = new CardRegistrationDto();
-    cardRegistrationDto.setAccountId(5L);
-    cardRegistrationDto.setFirstTwelveNumbers("123456789012");
-    cardRegistrationDto.setLastFourNumbers("4567");
-    cardRegistrationDto.setValidFromDate(LocalDate.of(2021, 10, 23));
-    cardRegistrationDto.setExpireDate(LocalDate.of(2024, 10, 23));
-    cardRegistrationDto.setHolderName("Ivanov Ivan Ivanovich");
-    cardRegistrationDto.setPaymentSystem("VISA");
-    cardRegistrationDto.setTypeName("SILVER");
-    return cardRegistrationDto;
-  }
+        return card;
+    }
 
-  public static CardResponseDto populateCardResponseDto() {
-    CardResponseDto cardResponseDto = new CardResponseDto();
-    cardResponseDto.setId(17L);
-    cardResponseDto.setAccountId(5L);
-    cardResponseDto.setLastFourNumbers("4567");
-    cardResponseDto.setValidFromDate(LocalDate.of(2021, 10, 23));
-    cardResponseDto.setExpireDate(LocalDate.of(2024, 10, 23));
-    cardResponseDto.setHolderName("Ivanov Ivan Ivanovich");
-    cardResponseDto.setPaymentSystem("VISA");
-    cardResponseDto.setTypeName("SILVER");
-    return cardResponseDto;
-  }
+    public static CardUpdateDto populateCardUpdateDto() {
+        CardUpdateDto cardUpdateDto = new CardUpdateDto();
+        cardUpdateDto.setId(17L);
+        cardUpdateDto.setAccountId(5L);
+        cardUpdateDto.setFirstTwelveNumbers("123456789012");
+        cardUpdateDto.setLastFourNumbers("4567");
+        cardUpdateDto.setValidFromDate(LocalDate.of(2021, 10, 23));
+        cardUpdateDto.setExpireDate(LocalDate.of(2024, 10, 23));
+        cardUpdateDto.setHolderName("Ivanov Ivan Ivanovich");
+        cardUpdateDto.setPaymentSystem("VISA");
+        cardUpdateDto.setTypeName("SILVER");
+        return cardUpdateDto;
+    }
 
-  public static Account populateAccount(Account account) {
-    account.setId(5L);
-    return account;
-  }
+    public static CardRegistrationDto populateCardRegistrationDto() {
+        CardRegistrationDto cardRegistrationDto = new CardRegistrationDto();
+        cardRegistrationDto.setAccountId(5L);
+        cardRegistrationDto.setFirstTwelveNumbers("123456789012");
+        cardRegistrationDto.setLastFourNumbers("4567");
+        cardRegistrationDto.setValidFromDate(LocalDate.of(2021, 10, 23));
+        cardRegistrationDto.setExpireDate(LocalDate.of(2024, 10, 23));
+        cardRegistrationDto.setHolderName("Ivanov Ivan Ivanovich");
+        cardRegistrationDto.setPaymentSystem("VISA");
+        cardRegistrationDto.setTypeName("SILVER");
+        return cardRegistrationDto;
+    }
 
-  public static TypeCard populateTypeCard() {
-    TypeCard typeCard = new TypeCard();
-    typeCard.setId(1L);
-    typeCard.setPaymentSystem("VISA");
-    typeCard.setTypeName("SILVER");
-    return typeCard;
-  }
+    public static CardResponseDto populateCardResponseDto() {
+        CardResponseDto cardResponseDto = new CardResponseDto();
+        cardResponseDto.setId(17L);
+        cardResponseDto.setAccountId(5L);
+        cardResponseDto.setLastFourNumbers("4567");
+        cardResponseDto.setValidFromDate(LocalDate.of(2021, 10, 23));
+        cardResponseDto.setExpireDate(LocalDate.of(2024, 10, 23));
+        cardResponseDto.setHolderName("Ivanov Ivan Ivanovich");
+        cardResponseDto.setPaymentSystem("VISA");
+        cardResponseDto.setTypeName("SILVER");
+        return cardResponseDto;
+    }
 
-  public static TypeCardResponseDto populateTypeCardResponseDto () {
-    TypeCardResponseDto typeCardResponseDto = new TypeCardResponseDto();
-    typeCardResponseDto.setId(1L);
-    typeCardResponseDto.setPaymentSystem("VISA");
-    typeCardResponseDto.setTypeName("SILVER");
-    return typeCardResponseDto;
-  }
+    public static Account populateAccount(Account account) {
+        account.setId(5L);
+        return account;
+    }
 
-  public static TypeCardUpdateDto populateTypeCardUpdateDto () {
-    TypeCardUpdateDto typeCardUpdateDto = new TypeCardUpdateDto();
-    typeCardUpdateDto.setId(1L);
-    typeCardUpdateDto.setPaymentSystem("VISA");
-    typeCardUpdateDto.setTypeName("SILVER");
-    return typeCardUpdateDto;
-  }
+    public static Account populateAccountWithBalanceAndCurrency(Account a) {
+        Account account = populateAccount(a);
+        account.setBalance(0);
+        account.setCurrency("BEL");
+
+        return account;
+    }
+
+    public static TypeCard populateTypeCard(TypeCard typeCard) {
+        typeCard.setId(1L);
+        typeCard.setPaymentSystem("VISA");
+        typeCard.setTypeName("SILVER");
+
+        return typeCard;
+    }
+
+    public static TypeCard populateTypeCard() {
+        TypeCard typeCard = new TypeCard();
+        typeCard.setId(1L);
+        typeCard.setPaymentSystem("VISA");
+        typeCard.setTypeName("SILVER");
+        return typeCard;
+    }
+
+    public static TypeCardResponseDto populateTypeCardResponseDto() {
+        TypeCardResponseDto typeCardResponseDto = new TypeCardResponseDto();
+        typeCardResponseDto.setId(1L);
+        typeCardResponseDto.setPaymentSystem("VISA");
+        typeCardResponseDto.setTypeName("SILVER");
+        return typeCardResponseDto;
+    }
+
+    public static TypeCardUpdateDto populateTypeCardUpdateDto() {
+        TypeCardUpdateDto typeCardUpdateDto = new TypeCardUpdateDto();
+        typeCardUpdateDto.setId(1L);
+        typeCardUpdateDto.setPaymentSystem("VISA");
+        typeCardUpdateDto.setTypeName("SILVER");
+        return typeCardUpdateDto;
+    }
 
 }
