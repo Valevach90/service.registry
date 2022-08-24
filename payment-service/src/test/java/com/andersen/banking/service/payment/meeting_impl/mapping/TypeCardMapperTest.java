6f6d491ced7e01403e1940671e5e7d263881a97c
a.dzyubak <GxXqLoLa2SED>
2022-08-24 16:01:20 +0300

    solve issue with transfer open api config

--- payment-service/src/test/java/com/andersen/banking/service/payment/meeting_impl/mapping/TypeCardMapperTest.java
+++ payment-service/src/test/java/com/andersen/banking/service/payment/meeting_impl/mapping/TypeCardMapperTest.java
@@ -20,7 +20,6 @@ public class TypeCardMapperTest {
     private TypeCard typeCard;
     private TypeCardResponseDto typeCardResponseDto;
     private TypeCardUpdateDto typeCardUpdateDto;
-
     @Autowired
     TypeCardMapper typeCardMapper;
 
