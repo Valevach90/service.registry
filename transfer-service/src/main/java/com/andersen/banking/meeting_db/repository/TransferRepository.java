dd48a0487f22a4c91182ab7f4e7c0a8752479832
Fulkin <sea-xv@mail.ru>
2022-08-25 18:58:17 +0300

    MRN-422 delete not need method

--- transfer-service/src/main/java/com/andersen/banking/meeting_db/repository/TransferRepository.java
+++ transfer-service/src/main/java/com/andersen/banking/meeting_db/repository/TransferRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -16,7 +15,5 @@ public interface TransferRepository extends JpaRepository<Transfer, UUID> {
     Optional<Transfer> findById(Long id);
 
     Page<Transfer> findByUserId(Long id, Pageable pageable);
-
-    List<Transfer> findTransfersByUserId(Long userId);
 
 }
