f39b42f42a1fd645a75f0aad80dd772efaff4d81
Siarhei Yurevich <s.yurevich@andersenlab.com>
2022-08-29 13:48:01 +0300

    populations types and currencies

--- deposit-service/src/main/resources/db/migration/V2__Currencies.sql
+++ deposit-service/src/main/resources/db/migration/V2__Currencies.sql
@@ -5,3 +5,8 @@ CREATE TABLE currencies
 
     CONSTRAINT pk_currencies PRIMARY KEY (id)
 );
+
+INSERT INTO currencies (id, name)
+VALUES (1, 'EUR');
+INSERT INTO currencies (id, name)
+VALUES (2, 'USD');
