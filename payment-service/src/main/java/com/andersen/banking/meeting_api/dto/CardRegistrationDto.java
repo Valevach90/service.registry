297dd299a7b1b9fb1e9886af62cf8635b9da6711
Fulkin <sea-xv@mail.ru>
2022-09-06 19:47:19 +0300

    MRN-401

--- payment-service/src/main/java/com/andersen/banking/meeting_api/dto/CardRegistrationDto.java
+++ payment-service/src/main/java/com/andersen/banking/meeting_api/dto/CardRegistrationDto.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.LocalDate;
 import java.util.UUID;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
@@ -18,39 +17,38 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class CardRegistrationDto {
 
-  @Min(1L)
-  @NotNull
-  @JsonProperty("account_id")
-  private UUID accountId;
+    @NotNull
+    @JsonProperty("account_id")
+    private UUID accountId;
 
-  @NotBlank
-  @JsonProperty("first_twelve_numbers")
-  @Pattern(regexp = "[0-9]{12}", message = "first_twelve_numbers should contain exactly 12 digits")
-  private String firstTwelveNumbers;
+    @NotBlank
+    @JsonProperty("first_twelve_numbers")
+    @Pattern(regexp = "[0-9]{12}", message = "first_twelve_numbers should contain exactly 12 digits")
+    private String firstTwelveNumbers;
 
-  @NotBlank
-  @JsonProperty("last_four_numbers")
-  @Pattern(regexp = "[0-9]{4}", message = "last_four_numbers should contain exactly 4 digits")
-  private String lastFourNumbers;
+    @NotBlank
+    @JsonProperty("last_four_numbers")
+    @Pattern(regexp = "[0-9]{4}", message = "last_four_numbers should contain exactly 4 digits")
+    private String lastFourNumbers;
 
-  @NotNull
-  @JsonProperty("valid_from_date")
-  private LocalDate validFromDate;
+    @NotNull
+    @JsonProperty("valid_from_date")
+    private LocalDate validFromDate;
 
-  @NotNull
-  @JsonProperty("expire_date")
-  private LocalDate expireDate;
+    @NotNull
+    @JsonProperty("expire_date")
+    private LocalDate expireDate;
 
-  @NotBlank
-  @Pattern(regexp = "[a-zA-Z- ]{3,30}", message = "message = \"holder_name should have at least 3 and at maximum 30 characters\"")
-  @JsonProperty("holder_name")
-  private String holderName;
+    @NotBlank
+    @Pattern(regexp = "[a-zA-Z- ]{3,30}", message = "message = \"holder_name should have at least 3 and at maximum 30 characters\"")
+    @JsonProperty("holder_name")
+    private String holderName;
 
-  @NotNull
-  @JsonProperty("payment_system")
-  private String paymentSystem;
+    @NotNull
+    @JsonProperty("payment_system")
+    private String paymentSystem;
 
-  @NotNull
-  @JsonProperty("type_name")
-  private String typeName;
+    @NotNull
+    @JsonProperty("type_name")
+    private String typeName;
 }
