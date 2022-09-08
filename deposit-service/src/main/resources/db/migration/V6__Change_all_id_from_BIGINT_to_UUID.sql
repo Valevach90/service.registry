23172be422388a3ee79e4ddd3e779065c1f514b7
arctg07 <arctg07@gmail.com>
2022-09-08 20:43:06 +0300

    Revert "Revert "MRN-665""
    This reverts commit ac3a4c571e42f885edd4f221dcf48be08b2bf1d3.

--- deposit-service/src/main/resources/db/migration/V6__Change_all_id_from_BIGINT_to_UUID.sql
+++ deposit-service/src/main/resources/db/migration/V6__Change_all_id_from_BIGINT_to_UUID.sql
@@ -1,33 +1,29 @@
 ALTER TABLE deposit_types
     DROP COLUMN id CASCADE,
-    ADD COLUMN id UUID UNIQUE DEFAULT gen_random_uuid() NOT NULL,
-    ADD CONSTRAINT pk_deposit_types PRIMARY KEY (id);
+    ADD COLUMN id UUID UNIQUE DEFAULT gen_random_uuid() NOT NULL;
 
 ALTER TABLE currencies
     DROP COLUMN id CASCADE,
-    ADD COLUMN id UUID UNIQUE DEFAULT gen_random_uuid() NOT NULL,
-    ADD CONSTRAINT pk_currencies PRIMARY KEY (id);
+    ADD COLUMN id UUID UNIQUE DEFAULT gen_random_uuid() NOT NULL;
 
 ALTER TABLE deposit_products
-    DROP COLUMN id CASCADE ,
+    DROP COLUMN id CASCADE,
     ADD COLUMN id UUID UNIQUE DEFAULT gen_random_uuid() NOT NULL,
-    ADD CONSTRAINT pk_deposits_products PRIMARY KEY (id),
 
-    DROP COLUMN deposit_type_id CASCADE ,
+    DROP COLUMN deposit_type_id CASCADE,
     ADD COLUMN deposit_type_id UUID NOT NULL,
 
-    DROP COLUMN currency_id CASCADE ,
+    DROP COLUMN currency_id CASCADE,
     ADD COLUMN currency_id UUID NOT NULL;
 
 ALTER TABLE deposits
-    DROP COLUMN id CASCADE ,
+    DROP COLUMN id CASCADE,
     ADD COLUMN id UUID UNIQUE DEFAULT gen_random_uuid() NOT NULL,
-    ADD CONSTRAINT pk_deposits PRIMARY KEY (id),
 
-    DROP COLUMN deposit_product_id CASCADE ,
+    DROP COLUMN deposit_product_id CASCADE,
     ADD COLUMN deposit_product_id UUID NOT NULL,
 
-    DROP COLUMN deposit_type_id CASCADE ,
+    DROP COLUMN deposit_type_id CASCADE,
     ADD COLUMN deposit_type_id UUID NOT NULL,
 
     DROP COLUMN currency_id CASCADE,
@@ -42,6 +38,18 @@ ALTER TABLE transfers
 
     DROP COLUMN deposit_id CASCADE ,
     ADD COLUMN deposit_id UUID NOT NULL;
+
+ALTER TABLE deposit_types
+    ADD CONSTRAINT pk_deposit_types PRIMARY KEY (id);
+
+ALTER TABLE currencies
+    ADD CONSTRAINT pk_currencies PRIMARY KEY (id);
+
+ALTER TABLE deposit_products
+    ADD CONSTRAINT pk_deposits_products PRIMARY KEY (id);
+
+ALTER TABLE deposits
+    ADD CONSTRAINT pk_deposits PRIMARY KEY (id);
 
 ALTER TABLE deposit_products
     ADD CONSTRAINT fk_deposit_products_on_deposit_types FOREIGN KEY (deposit_type_id) REFERENCES deposit_types (id),
