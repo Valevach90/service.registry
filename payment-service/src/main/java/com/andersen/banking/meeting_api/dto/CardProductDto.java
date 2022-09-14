9dcb2bcbd13980d6c9f752c68ea8d5022350bf18
Даниль <@DESKTOP-N8194Q8>
2022-09-14 11:24:47 +0300

    WIP

--- payment-service/src/main/java/com/andersen/banking/meeting_api/dto/CardProductDto.java
+++ payment-service/src/main/java/com/andersen/banking/meeting_api/dto/CardProductDto.java
@@ -2,6 +2,7 @@ package com.andersen.banking.meeting_api.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.UUID;
 import lombok.Data;
 
 @Data
@@ -9,4 +10,15 @@ import lombok.Data;
 @Schema(description = "dto for card product")
 public class CardProductDto {
 
+    private UUID id;
+
+    private int maxCashback;
+
+    private double price;
+
+    private String advantages;
+
+    private String bankPartners;
+
+    private String loyaltyProgram;
 }
