c77a7117b9c106c2fa7daebdfb20be747037430c
Yulia Suslova <suslova.dzhulia2017@yandex.ru>
2022-09-15 17:01:36 +0300

    change id currency type delete gitignore

--- credit-service/src/main/resources/db.migration/V2__currency.sql
+++ credit-service/src/main/resources/db.migration/V2__currency.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS currency
 (
-    id                          BIGINT,
-    name                        VARCHAR(3)                             NOT NULL,
+    id   UUID DEFAULT gen_random_uuid() NOT NULL,
+    name VARCHAR(3)                     NOT NULL,
 
     CONSTRAINT pk_currencies PRIMARY KEY (id)
 );
