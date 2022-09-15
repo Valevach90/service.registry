fffdafd5adfa64acddaee65b939afad1f49c3bd0
Yulia Suslova <suslova.dzhulia2017@yandex.ru>
2022-09-15 22:00:21 +0300

    fix script

--- credit-service/src/main/resources/db.migration/V2_credit_product.sql
+++ credit-service/src/main/resources/db.migration/V2_credit_product.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS credit_products
 (
-    id                    UUID DEFAULT gen_random_uuid() NOT NULL,
-    product_name          VARCHAR(255)                   NOT NULL,
+    id                    UUID PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+    product_name          VARCHAR(255)                               NOT NULL,
     min_sum               DECIMAL,
     max_sum               DECIMAL,
-    currency_id           BIGINT                         NOT NULL,
+    currency_id           UUID                                       NOT NULL,
     min_loan_rate         DECIMAL,
     max_loan_rate         DECIMAL,
     need_guarantee        BOOLEAN,
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS credit_products
     min_term              INTEGER,
     max_term              INTEGER,
     description           TEXT,
-    calculation_mode      VARCHAR(255)                   NOT NULL,
+    calculation_mode      VARCHAR(255)                               NOT NULL,
     grace_period_month    INTEGER,
     need_income_statement BOOLEAN
 );
