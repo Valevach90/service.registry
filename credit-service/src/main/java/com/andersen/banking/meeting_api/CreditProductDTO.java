5138d531d53d25b2d6e4d67c8612ad8d0ff5e923
Yulia Suslova <suslova.dzhulia2017@yandex.ru>
2022-09-15 12:14:43 +0300

    add classes and change application.yml

--- credit-service/src/main/java/com/andersen/banking/meeting_api/CreditProductDTO.java
+++ credit-service/src/main/java/com/andersen/banking/meeting_api/CreditProductDTO.java
@@ -1,5 +1,64 @@
 package com.andersen.banking.meeting_api;
 
+import com.andersen.banking.CalculationMode;
+import com.andersen.banking.meeting_db.Currency;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.math.BigDecimal;
+import java.util.UUID;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Schema(description = "dto for credit product")
 public class CreditProductDTO {
 
+    @Id
+    private UUID uuid;
+
+
+    private String name;
+
+    private BigDecimal minSum;
+
+
+    private BigDecimal maxSum;
+
+
+
+    private Currency currency;
+
+
+    private BigDecimal minLoanRate;
+
+
+    private BigDecimal maxLoanRate;
+
+
+    private Boolean needGuarantee;
+
+
+    private Boolean earlyRepayment;
+
+
+    private Integer minTerm;
+
+
+    private Integer maxTerm;
+
+
+    private String description;
+
+
+    @Enumerated(EnumType.STRING)
+    private CalculationMode calculationMode;
+
+
+    private Integer gracePeriodMonth;
+
+
+    private Boolean needIncomeStatement;
 }
---
+++ credit-service/src/main/java/com/andersen/banking/meeting_db/Currency.java
@@ -0,0 +1,25 @@
+package com.andersen.banking.meeting_db;
+
+import java.util.UUID;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name = "currencies")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Currency {
+
+    @Id
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+}
