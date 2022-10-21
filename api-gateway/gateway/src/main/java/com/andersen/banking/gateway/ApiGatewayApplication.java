666332fd5de7fcea1d1b8ea419a5d03615b34beb
Fulkin <sea-xv@mail.ru>
2022-10-21 21:08:45 +0300

    MRN-401 fix gateway

--- api-gateway/gateway/src/main/java/com/andersen/banking/gateway/ApiGatewayApplication.java
+++ api-gateway/gateway/src/main/java/com/andersen/banking/gateway/ApiGatewayApplication.java
@@ -3,10 +3,8 @@ package com.andersen.banking.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import reactivefeign.spring.config.EnableReactiveFeignClients;
 
 @SpringBootApplication
-@EnableReactiveFeignClients
 @EnableEurekaClient
 public class ApiGatewayApplication {
 
